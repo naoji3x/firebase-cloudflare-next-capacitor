412d8acb13b756a41d9c111100c002b4f6970bdb
naoji3x <naoji.3x@gmail.com>
2024-05-02 10:10:49 +0000

    feat: add auth and storybook

---
+++ apps/frontend/src/env.mjs
@@ -0,0 +1,40 @@
+import { createEnv } from '@t3-oss/env-nextjs'
+import { z } from 'zod'
+
+export const env = createEnv({
+  server: {
+    NEXTAUTH_SECRET: z.string(),
+    GOOGLE_ID: z.string(),
+    GOOGLE_SECRET: z.string()
+    // VERSION: z.string()
+  },
+  client: {
+    NEXT_PUBLIC_APP_URL: z.string(),
+    NEXT_PUBLIC_API_KEY: z.string(),
+    NEXT_PUBLIC_AUTH_DOMAIN: z.string(),
+    NEXT_PUBLIC_PROJECT_ID: z.string(),
+    NEXT_PUBLIC_STORAGE_BUCKET: z.string(),
+    NEXT_PUBLIC_MESSAGING_SENDER_ID: z.string(),
+    NEXT_PUBLIC_APP_ID: z.string(),
+    // NEXT_PUBLIC_VERSION: z.string(),
+    NEXT_PUBLIC_USE_FIREBASE_EMULATOR: z.string().optional()
+  },
+
+  runtimeEnv: {
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+    GOOGLE_ID: process.env.GOOGLE_ID,
+    GOOGLE_SECRET: process.env.GOOGLE_SECRET,
+
+    NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
+    NEXT_PUBLIC_API_KEY: process.env.NEXT_PUBLIC_API_KEY,
+    NEXT_PUBLIC_AUTH_DOMAIN: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+    NEXT_PUBLIC_PROJECT_ID: process.env.NEXT_PUBLIC_PROJECT_ID,
+    NEXT_PUBLIC_STORAGE_BUCKET: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+    NEXT_PUBLIC_MESSAGING_SENDER_ID:
+      process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+    NEXT_PUBLIC_APP_ID: process.env.NEXT_PUBLIC_APP_ID,
+    // NEXT_PUBLIC_VERSION: process.env.VERSION,
+    NEXT_PUBLIC_USE_FIREBASE_EMULATOR:
+      process.env.NEXT_PUBLIC_USE_FIREBASE_EMULATOR
+  }
+})
