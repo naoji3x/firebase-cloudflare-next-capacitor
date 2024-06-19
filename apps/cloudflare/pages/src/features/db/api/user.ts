733b5fee48fbe7a5f61b357c451c0eb8d8ed09c6
naoji3x <naoji.3x@gmail.com>
2024-06-19 14:07:47 +0000

    refactor: remove cloudflare/pages

--- apps/cloudflare/pages/src/features/db/api/user.ts
+++
@@ -1,9 +0,0 @@
-import { firestore } from '@/firebase/client'
-import { arrayUnion, doc, setDoc } from 'firebase/firestore'
-
-const collectionName = (uid: string) => `users/${uid}`
-
-export const upsertToken = async (uid: string, token: string) => {
-  const docRef = doc(firestore, collectionName(uid))
-  await setDoc(docRef, { tokens: arrayUnion(token) }, { merge: true })
-}
