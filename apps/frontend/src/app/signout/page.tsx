f03c004b63cfe22380d4226770239e57bb81629d
naoji3x <naoji.3x@gmail.com>
2024-06-23 02:28:00 +0000

    fix: sign out page bug

--- apps/frontend/src/app/signout/page.tsx
+++ apps/frontend/src/app/signout/page.tsx
@@ -6,13 +6,13 @@ import { useEffect } from 'react'
 // firestoreのsnapshotを使っている画面等で、直接singOut関数を呼び出すと、
 // unsubscribeが呼ばれずにログアウトしてエラーになるため、一度このページにリダイレクトしてからログアウト処理を行う。
 //
-const SignOut = ({ redirect = '/' }: { redirect: string }) => {
+const SignOut = () => {
   useEffect(() => {
     const func = async () => {
-      await signOut(redirect)
+      await signOut()
     }
     func()
-  }, [redirect])
+  }, [])
 
   return (
     <div
